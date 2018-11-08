bdce8791b98830b3541cb5f6bfe6e48aee785577
Stefan Tolksdorf <stefan.tolksdorf@gmail.com>
2018-11-08 10:57:28 +0100

    First Commit

---
+++ main.js
@@ -0,0 +1,37 @@
+class ListBinding {
+  constructor(element) {
+    this.element = element;
+    this.textList = [];
+    this.doneList = [];
+  }
+  deleteAll() {
+    while (this.element.firstChild)
+      this.element.removeChild(this.element.firstChild);
+  }
+  update() {
+    this.deleteAll();
+    for (let text of this.textList) {
+      let item = document.createElement("li");
+      item.textContent = text;
+      this.element.appendChild(item);
+    }
+  }
+  add(item) {
+    this.textList.push(item);
+    this.update();
+  }
+  deleteFirst() {
+    this.textList.shift();
+    this.update();
+  }
+  deleteLast() {
+    this.textList.pop();
+    this.update();
+  }
+}
+
+const myList = document.getElementById("myList");
+const doneList = document.getElementById("doneList");
+var firstList = new ListBinding(myList);
+var secondList = new Listbinding(doneList);
+secondList.add("Cake");
