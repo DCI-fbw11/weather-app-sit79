854ffa5501802e4fd5465c36bd5848194d7d4c20
Stefan Tolksdorf <stefan.tolksdorf@gmail.com>
2018-11-12 15:55:31 +0100

    complete rebuild by Günes

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,68 +1,104 @@
 class ListBinding {
   constructor(element) {
     this.element = element;
-    this.textList = [];
+    this.DOMtodo = document.getElementById("toDoList");
+    this.DOMdone = document.getElementById("doneList");
+    this.clearButton = document.getElementById("clearButton");
+    this.toDoList = [];
     this.doneList = [];
   }
   deleteAll() {
-    while (this.element.firstChild)
-      this.element.removeChild(this.element.firstChild);
+    this.DOMdone.innerHTML = "";
+    this.DOMtodo.innerHTML = "";
   }
-  update() {
+
+  render() {
     this.deleteAll();
     let i = 0;
-    for (let text of this.textList) {
+    for (let text of this.toDoList) {
       let item = document.createElement("li");
-      let content = `<p class="done" id="entry${i}" onclick="firstList.isDone(${i})">${text}</p>`;
-      item.innerHTML = content;
-      this.element.appendChild(item);
+      let content;
+      if (text.status === "unchecked") {
+        item.className = "todo";
+        item.setAttribute("onclick", `isDone(${i})`);
+        //text.status = "unchecked";
+        item.innerHTML = text.input;
+        this.DOMtodo.appendChild(item);
+        // content = `<p class="deleted" id="entry${i}" onclick="myTasks.isDone(${i})">${
+        // }</p>`;
+      } else {
+        item.className = "done";
+        item.setAttribute("onclick", `isDone(${i})`);
+        // text.status = "checked";
+        item.innerHTML = text.input;
+        this.DOMdone.appendChild(item);
+        // content = `<p class="done" id="entry${i}" onclick="myTasks.isDone(${i})">${
+        // }</p>`;
+      }
+      //this.element.appendChild(item);
       i++;
     }
     let position = document.getElementById("newEntryField");
     position.value = "";
+    if (this.toDoList.length) {
+      for (let item of this.toDoList) {
+        if (item.status === "checked") {
+          this.clearButton.style.display = "inline-block";
+          break;
+        } else {
+          this.clearButton.style.display = "none";
+        }
+      }
+    } else {
+      this.clearButton.style.display = "none";
+    }
   }
   add(item) {
     if (item !== "") {
-      this.textList.push(item);
-      this.update();
+      this.toDoList.push({ input: item, status: "unchecked" });
+      this.render();
     } else {
-      window.alert(
-        "Please enter a task"
-      );
+      window.alert("Please enter a task");
     }
   }
-  deleteFirst() {
-    this.textList.shift();
-    this.update();
-  }
-  deleteLast() {
-    this.textList.pop();
-    this.update();
-  }
   isDone(index) {
-    let tempVal = `entry${index}`;
-    let listItem = document.getElementById(tempVal);
-    listItem.classList.remove('done');
-    listItem.classList.add('deleted');
-    setTimeout(function () {
-      // console.log(firstList.element);
-      let tempValue = firstList.textList.splice(index, 1);
-      firstList.doneList.push(tempValue);
-      firstList.update();
-    }, 1500);
+    if (this.toDoList[index].status === "checked") {
+      this.toDoList[index].status = "unchecked";
+    } else {
+      this.toDoList[index].status = "checked";
+    }
+    this.render();
+  }
+  clearList() {
+    for (let i = 0; i < this.toDoList.length; i++) {
+      if (this.toDoList[i].status === "checked") {
+        this.toDoList.splice(i, 1);
+        i--;
+      }
+    }
+    this.render();
   }
 }
 
-const myList = document.getElementById("myList");
+const listDiv = document.getElementById("listDiv");
+const myList = document.getElementById("toDoList");
 const doneList = document.getElementById("doneList");
-var firstList = new ListBinding(myList);
+var myTasks = new ListBinding(listDiv);
 
 var newEntry = document.getElementById("newEntry");
 var submitButton = document.getElementById("submitButton");
 
-submitButton.addEventListener("click", function (e) {
+submitButton.addEventListener("click", function(e) {
   e.preventDefault();
   let position = document.getElementById("newEntryField");
   let entry = position.value;
-  firstList.add(entry);
+  myTasks.add(entry);
 });
+
+function isDone(item) {
+  myTasks.isDone(item);
+}
+
+function clearList() {
+  myTasks.clearList();
+}
